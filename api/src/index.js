fa50d05983adc6d867f553a841d937e5474106cd
Spoty-O <korotysh16@ukr.net>
2025-03-26 17:24:34 +0200

    zdorova

--- api/src/index.js
+++ api/src/index.js
@@ -2,15 +2,27 @@ const express = require('express')
 const { Sequelize, DataTypes } = require('@sequelize/core')
 const bodyParser = require('body-parser')
 const path = require('path');
+const { PostgresDialect } = require('@sequelize/postgres')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
 const HOST = process.env.HOST || "localhost"
-const DB_URL = process.env.DB_URL || "postgresql://postgres:123456qwe!@#@db.vttqlbnhpjphsbaptidb.supabase.co:5432/postgres"
+const DB_URL = process.env.DB_URL || "postgresql://postgres.vttqlbnhpjphsbaptidb:123456qwe!@#@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
 
 const sequelize = new Sequelize({
-  url: process.env.DB_URL, // Указывайте URL подключения здесь
-  dialect: 'postgres',
+  // url: process.env.DB_URL, // Указывайте URL подключения здесь
+  dialect: PostgresDialect,
   password: '123456qwe!@#',
+  host: 'aws-0-eu-central-1.pooler.supabase.com',
+  port: 6543,
+  database: 'postgres',
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000
+  // },
+  user: 'postgres.vttqlbnhpjphsbaptidb',
 });
 
 const User = sequelize.define('User', {
@@ -24,10 +36,19 @@ const User = sequelize.define('User', {
     type: DataTypes.STRING,
   },
 });
-const PATH = path.join(process.cwd(), 'frontend/dist')
+const PATH = path.join(process.cwd(), '../frontend/dist')
 app = express()
 app.use(bodyParser.json())
 app.use(express.static(PATH));
+const corsOptions = {
+  origin: ['http://localhost'],
+  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+  credentials: true,
+  enablePreflight: true
+}
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions))
 
 app.get('/', async (req, res) => {
   res.sendFile(path.join(PATH, 'index.html'));
